3e100d7760b080c6ca8fe92a2ff7dbfdbbe54a28
Atsushi <atsushi@moongift.jp>
2022-07-05 22:36:55 +0900

    Initial Commit

---
+++ translationApp/translationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  translationAppApp.swift
+//  translationApp
+//
+//  Created by Atsushi on 2022/07/05.
+//
+
+import SwiftUI
+
+@main
+struct translationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
