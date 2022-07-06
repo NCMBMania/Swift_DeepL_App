7b0cb2826e7e956eaa9aaa038c2a8677963c9ae0
Atsushi <atsushi@moongift.jp>
2022-07-06 23:30:06 +0900

    Init

--- translationApp/ContentView.swift
+++ translationApp/ContentView.swift
@@ -1,16 +1,32 @@
 //
 //  ContentView.swift
-//  translationApp
+//  translate
 //
 //  Created by Atsushi on 2022/07/05.
 //
 
 import SwiftUI
+import NCMB
 
+// タブを表示
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+                    TranslateView()
+                        .tabItem {
+                            VStack {
+                                Image(systemName: "textformat")
+                                Text("翻訳")
+                            }
+                    }.tag(1)
+                    HistoryView()
+                        .tabItem {
+                            VStack {
+                                Image(systemName: "list.bullet")
+                                Text("履歴")
+                            }
+                    }.tag(2)
+                }
     }
 }
 
--- translationApp/translationAppApp.swift
+++ translationApp/translationAppApp.swift
@@ -6,9 +6,17 @@
 //
 
 import SwiftUI
+import NCMB
 
 @main
 struct translationAppApp: App {
+    init() {
+        let applicationKey = KeyManager().getValue(key: "ApplicationKey") as! String
+        let clientKey = KeyManager().getValue(key: "ClientKey") as! String
+        // APIキーの設定とSDK初期化
+        NCMB.initialize(applicationKey: applicationKey, clientKey: clientKey)
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
